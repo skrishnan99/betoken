791a68998dc0aa9ee2ec9b06a06992424cce54f9
Avinash Raja <avinashraja98@gmail.com>
2018-05-26 16:47:22 -0230

    added body.js and betoken object

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Betoken } from "./objects/betoken.js";
+
+import init from "./body";
+
+init();
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
