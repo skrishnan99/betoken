c47d3f05e530afb59595c69a35966bc5454d23b5
Avinash Raja <avinashraja98@gmail.com>
2018-05-26 21:34:39 -0230

    cleared unwanted files

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-
-import { Betoken } from "./objects/betoken.js";
 
 import { init } from "./body";
 
 init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
